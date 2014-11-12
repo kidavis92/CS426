fa4b394a5e6a55f297d191ed9b8740d1bf1c370c
kidavis92 <boneydavis@yahoo.com>
2014-11-11 22:05:13 -0700

    Renamed Formal Param, added brace to end of program

--- Davis_compiler/SemanticAnalyzer.cs
+++ Davis_compiler/SemanticAnalyzer.cs
@@ -30,19 +30,19 @@ namespace Davis_compiler
 
         // override the case to change the order, this way processes them
         // backwards (for no good reason)
-//        public override void CaseAMoreDecls(comp5210.node.AM node)
-//        {
-//            InAMoreDecls(node);
-//            if (node.GetDecl() != null)
-//            {
-//                node.GetDecl().Apply(this);
-//           }
-//            if (node.GetDecls() != null)
-//            {
-//                node.GetDecls().Apply(this);
-//            }
-//            OutAMoreDecls(node);
-        }
+        //        public override void CaseAMoreDecls(comp5210.node.AM node)
+        //        {
+        //            InAMoreDecls(node);
+        //            if (node.GetDecl() != null)
+        //            {
+        //                node.GetDecl().Apply(this);
+        //           }
+        //            if (node.GetDecls() != null)
+        //            {
+        //                node.GetDecls().Apply(this);
+        //            }
+        //            OutAMoreDecls(node);
+        //        }
 
         // begin our work
         public override void OutAProgram(comp5210.node.AProgram node)
@@ -76,14 +76,14 @@ namespace Davis_compiler
         {
             base.OutANoneFormalParam(node);
         }
-        public override void OutANoneformalparm(comp5210.node.ANoneformaparm node)
+        public override void OutANoneformalparam(comp5210.node.ANoneFormalParam node)
         {
         }
         //fix
-        public override void OutATypeSecondparam(comp5210.node.ATypeSecondparam node)
+        public override void OutATypeSecondParam(comp5210.node.ATypeSecondParam node)
         {
         }
-        public override void OutANoneSecondparam(comp5210.node.ANoneSecondparam node)
+        public override void OutANoneSecondParam(comp5210.node.ANoneSecondParam node)
         {
         }
         //end fix
@@ -139,117 +139,118 @@ namespace Davis_compiler
         {
             base.OutAWhileState(node);
         }
-        
+
         // end our work
 
 
         // 
-//        public override void OutADecl(comp5210.node.ADecl node)
-//        {
-//            string typename = node.GetTypename().Text;
-//            string varname = node.GetVarname().Text;
-//            Definition typedefn;
-//            // lookup the type
-//            if (!stringhash.TryGetValue(typename, out typedefn))
-//            {
-//                Console.WriteLine("[" + node.GetTypename().Line + "]: " +
-//                    typename + " is not defined.");
- //           }
-            // check to make sure what we got back is a type
-//            else if (!(typedefn is TypeDefinition))
-//            {
-//                Console.WriteLine("[" + node.GetSemicolon().Line + "]: " +
-//                    typename + " is an invalid type.");
- //           }
-//            else
-//            {
-                // add this variable to the hash table
-                // note you need to add checks to make sure this 
-                // variable name isn't already defined look it up in the sringhash.
- //               VariableDefinition vardefn = new VariableDefinition();
- //               vardefn.name = varname;
- //               vardefn.vartype = typedefn as TypeDefinition;
-//                stringhash.Add(vardefn.name, vardefn);
- //           }
- //       }
-//        public override void OutAStmt(comp5210.node.AStmt node)
- //       {
- //           Definition rhs, lhs;
- //           nodehash.TryGetValue(node.GetExpr(), out rhs);
- //           stringhash.TryGetValue(node.GetId().Text, out lhs);
- //           // make sure left hand side and right hand side match
- //           // of course, you should really make sure left side is
- //           // a variable first make sure lhs is a variable definition
-//            if ((lhs as VariableDefinition).vartype != rhs)
-//            {
-//                Console.WriteLine("[" + node.GetEquals().Line + "]: " +
-//                    "types don't match");
-//            }
-//        }
-//        public override void OutAComplexExpr(comp5210.node.AComplexExpr node)
- //       {
- //           Definition lhs;
- //           nodehash.TryGetValue(node.GetBase(), out lhs);
-            // you should really get the types of both sides and make sure 
-            // they match
-            // make sure the type of the child is a BasicType, as those
-            // are the only addable things.
- //           nodehash.Add(node, lhs);
- //       }
- //       public override void OutASimpleExpr(comp5210.node.ASimpleExpr node)
- //       {
-  //          Definition lhs;
- //           nodehash.TryGetValue(node.GetBase(), out lhs);
- //           nodehash.Add(node, lhs);
-//        }
-//        public override void OutAParenBase(comp5210.node.AParenBase node)
-//        {
-//            Definition exprdefn;
- //           nodehash.TryGetValue(node.GetExpr(), out exprdefn);
- //           nodehash.Add(node, exprdefn);
-//        }
-//        public override void OutAIdBase(comp5210.node.AIdBase node)
- //       {
- //           Definition iddefn;
- //           if (!stringhash.TryGetValue(node.GetId().Text, out iddefn))
-  //          {
-//                Console.WriteLine("[" + node.GetId().Line + "]: " +
- //                   node.GetId().Text + " is not defined");
- //           }
-            // you should really make sure that iddefn is a variable 
-            // definition
-//            else
-//            {
-//                nodehash.Add(node, (iddefn as VariableDefinition).vartype);
-//            }
-//        }
-//        public override void OutAOthersMoreParam(comp5210.node.AOthersMorePa node)
-//        {
-        
- //           string varname = node.GetVar().Text;
-//            Definition typedefn;
-            // lookup the type
-//            if (!stringhash.TryGetValue(typename, out typedefn))
-//            {
- //               Console.WriteLine("[" + node.GetTypename().Line + "]: " +
-//                    typename + " is not defined.");
- //           }
-            // check to make sure what we got back is a type
-//            else if (!(typedefn is TypeDefinition))
-//            {
-//                Console.WriteLine("[" + node.GetSemicolon().Line + "]: " +
- //                   typename + " is an invalid type.");
- //           }
- //           else
- //           {
-                // add this variable to the hash table
-                // note you need to add checks to make sure this 
-                // variable name isn't already defined look it up in the sringhash.
-//                VariableDefinition vardefn = new VariableDefinition();
-//                vardefn.name = varname;
- //               vardefn.vartype = typedefn as TypeDefinition;
- //               stringhash.Add(vardefn.name, vardefn);
- //           }
- //       }
- //   }
+        //        public override void OutADecl(comp5210.node.ADecl node)
+        //        {
+        //            string typename = node.GetTypename().Text;
+        //            string varname = node.GetVarname().Text;
+        //            Definition typedefn;
+        //            // lookup the type
+        //            if (!stringhash.TryGetValue(typename, out typedefn))
+        //            {
+        //                Console.WriteLine("[" + node.GetTypename().Line + "]: " +
+        //                    typename + " is not defined.");
+        //           }
+        // check to make sure what we got back is a type
+        //            else if (!(typedefn is TypeDefinition))
+        //            {
+        //                Console.WriteLine("[" + node.GetSemicolon().Line + "]: " +
+        //                    typename + " is an invalid type.");
+        //           }
+        //            else
+        //            {
+        // add this variable to the hash table
+        // note you need to add checks to make sure this 
+        // variable name isn't already defined look it up in the sringhash.
+        //               VariableDefinition vardefn = new VariableDefinition();
+        //               vardefn.name = varname;
+        //               vardefn.vartype = typedefn as TypeDefinition;
+        //                stringhash.Add(vardefn.name, vardefn);
+        //           }
+        //       }
+        //        public override void OutAStmt(comp5210.node.AStmt node)
+        //       {
+        //           Definition rhs, lhs;
+        //           nodehash.TryGetValue(node.GetExpr(), out rhs);
+        //           stringhash.TryGetValue(node.GetId().Text, out lhs);
+        //           // make sure left hand side and right hand side match
+        //           // of course, you should really make sure left side is
+        //           // a variable first make sure lhs is a variable definition
+        //            if ((lhs as VariableDefinition).vartype != rhs)
+        //            {
+        //                Console.WriteLine("[" + node.GetEquals().Line + "]: " +
+        //                    "types don't match");
+        //            }
+        //        }
+        //        public override void OutAComplexExpr(comp5210.node.AComplexExpr node)
+        //       {
+        //           Definition lhs;
+        //           nodehash.TryGetValue(node.GetBase(), out lhs);
+        // you should really get the types of both sides and make sure 
+        // they match
+        // make sure the type of the child is a BasicType, as those
+        // are the only addable things.
+        //           nodehash.Add(node, lhs);
+        //       }
+        //       public override void OutASimpleExpr(comp5210.node.ASimpleExpr node)
+        //       {
+        //          Definition lhs;
+        //           nodehash.TryGetValue(node.GetBase(), out lhs);
+        //           nodehash.Add(node, lhs);
+        //        }
+        //        public override void OutAParenBase(comp5210.node.AParenBase node)
+        //        {
+        //            Definition exprdefn;
+        //           nodehash.TryGetValue(node.GetExpr(), out exprdefn);
+        //           nodehash.Add(node, exprdefn);
+        //        }
+        //        public override void OutAIdBase(comp5210.node.AIdBase node)
+        //       {
+        //           Definition iddefn;
+        //           if (!stringhash.TryGetValue(node.GetId().Text, out iddefn))
+        //          {
+        //                Console.WriteLine("[" + node.GetId().Line + "]: " +
+        //                   node.GetId().Text + " is not defined");
+        //           }
+        // you should really make sure that iddefn is a variable 
+        // definition
+        //            else
+        //            {
+        //                nodehash.Add(node, (iddefn as VariableDefinition).vartype);
+        //            }
+        //        }
+        //        public override void OutAOthersMoreParam(comp5210.node.AOthersMorePa node)
+        //        {
+
+        //           string varname = node.GetVar().Text;
+        //            Definition typedefn;
+        // lookup the type
+        //            if (!stringhash.TryGetValue(typename, out typedefn))
+        //            {
+        //               Console.WriteLine("[" + node.GetTypename().Line + "]: " +
+        //                    typename + " is not defined.");
+        //           }
+        // check to make sure what we got back is a type
+        //            else if (!(typedefn is TypeDefinition))
+        //            {
+        //                Console.WriteLine("[" + node.GetSemicolon().Line + "]: " +
+        //                   typename + " is an invalid type.");
+        //           }
+        //           else
+        //           {
+        // add this variable to the hash table
+        // note you need to add checks to make sure this 
+        // variable name isn't already defined look it up in the sringhash.
+        //                VariableDefinition vardefn = new VariableDefinition();
+        //                vardefn.name = varname;
+        //               vardefn.vartype = typedefn as TypeDefinition;
+        //               stringhash.Add(vardefn.name, vardefn);
+        //           }
+        //       }
+        //   }
+    }
 }
