b727c1fbfce86956f6c299bbcc19edf84dc11a0b
kidavis92 <boneydavis@yahoo.com>
2014-11-11 20:31:46 -0700

    Added SemanticAnalyzer, Added Definitons, copied Semantic Analyzer into new one, Changed parser. to Davis_Compiler.

---
+++ Davis_compiler/definitions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davis_compiler
+{
+    
+        public abstract class Definition
+        {
+            public string name;
+        }
+        public abstract class TypeDefinition : Definition
+        {
+        }
+        public class BasicType : TypeDefinition
+        {
+        }
+        public class VariableDefinition : Definition
+        {
+            public TypeDefinition vartype;
+        }
+    
+
+}
